0f40b0156254bf574a43714433b067832d48a028
gospargs <gospargs@gmail.com>
2022-03-09 20:40:28 +0100

    Update to solc 0.8.9.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,8 +1,10 @@
-pragma solidity ^0.4.17; // specifies the version of solidity that our code is written in
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9; // specifies the version of solidity that our code is written in
 
 contract Lottery {
     address public manager;  // does not enforece security if we use public or private
-    address[] public players; // dynamic array that can only hold addresses
+    address payable[] public players; // dynamic array that can only hold addresses
     address public winner;
     // msg is a global object that is always available in any function that runs in our contract
     /*
@@ -11,28 +13,29 @@ contract Lottery {
         msg.sender - Address of the account that started the current function invocation
         msg.value - Amount of ether (in wei) that was sent along with the function invocation
     */
-    function Lottery() public {
-        manager = msg.sender;  
+    constructor() {
+    manager = msg.sender;
     }
 
     // to enter the lottery ehter need to be sent
     function enter() public payable { // payable - function expects some ether
         require(msg.value > .01 ether); // used for validation, false - exits the function, true - continues to execute the function
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
 
     function random() private view returns (uint){ // returns random number - not realy that random
-       return uint(keccak256(block.difficulty,now,players)); // keccak256 - Global function,block - global variable (the difficulty to solve a current block), now - current time no need for importing
+               return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+               // keccak256 - Global function,block - global variable (the difficulty to solve a current block), now - current time no need for importing
     }
 
     function pickWinner () public restricted { // restircted - first calls the modifier restricted()
         uint index = random() % players.length;
-        players[index].transfer(this.balance); // transfer - used for sending money to se specific address from a contract, this.balance - this-is our contract
+        players[index].transfer(address(this).balance); // transfer - used for sending money to se specific address from a contract, this.balance - this-is our contract
         winner = players[index];
-        players = new address[](0); // empty out the list of players
+        players = new address payable[](0); // empty out the list of players
     }
 
-    function getPlayers() public view returns (address[]){
+    function getPlayers() public view returns (address payable[] memory){
         return players;
     }
 
